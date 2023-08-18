7cde5e6a2e7456e099fddd52e3646fc2f34f72ae
jadhavrahulrj01 <jadhavrahulrj01@gmail.com>
2023-08-18 20:32:14 +0530

    changes

--- resources/views/frontend/marketing-solution.blade.php
+++ resources/views/frontend/marketing-solution.blade.php
@@ -1,5 +1,5 @@
 @extends('frontend.layouts.app')
-@section('title', 'It Solution')
+@section('title', 'Marketing Solution')
 @section('page')
 
  {{-- <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css'> --}}
@@ -40,28 +40,29 @@
 
     <section class="container game-section">
         <div class="owl-carousel custom-carousel owl-theme">
-          <div class="item active" style="background-image: url(https://img.freepik.com/free-photo/rear-view-programmer-working-all-night-long_1098-18697.jpg?w=1380&t=st=1691845099~exp=1691845699~hmac=8f6ad3a6c923d17723040b39a5124d39f94bd790dbc63987c5c955549f1e2125);">
+          <div class="item active" style="background-image: url('user-assets/img/services/marketing-strategy.jpg') ;">
+
             <div class="item-desc">
               <h3>Marketing and Strategy</h3>
               <p>Achieving success through innovative marketing and strategic thinking.
             </p>
             </div>
           </div>
-          <div class="item" style="background-image: url(https://img.freepik.com/free-photo/html-css-collage-concept-with-person_23-2150062008.jpg?size=626&ext=jpg&ga=GA1.2.993756276.1691513559&semt=ais);">
+          <div class="item" style="background-image: url(user-assets/img/services/seo.jpg);">
             <div class="item-desc">
               <h3>SEO</h3>
               <p>Enhancing your online presence with expert SEO services.
             </p>
             </div>
           </div>
-          <div class="item" style="background-image: url(https://img.freepik.com/free-photo/programming-background-with-person-working-with-codes-computer_23-2150010125.jpg?size=626&ext=jpg&ga=GA1.2.993756276.1691513559&semt=ais);">
+          <div class="item" style="background-image: url(user-assets/img/services/smm.jpg);">
             <div class="item-desc">
               <h3>SMM</h3>
               <p>Enhance your brand's reach through dynamic social media management.
             </p>
             </div>
           </div>
-          <div class="item" style="background-image: url(https://img.freepik.com/free-photo/workers-it-company-working-computer_1303-19448.jpg?size=626&ext=jpg&ga=GA1.2.993756276.1691513559&semt=ais);">
+          <div class="item" style="background-image: url(user-assets/img/services/marketing-campaign.jpg);">
             <div class="item-desc">
               <h3>Marketing Campaign</h3>
               <p>Creating marketing campaigns that are results-driven and impactful.
